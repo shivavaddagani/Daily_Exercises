e212e1e4bbe3c8d96f2fc946e36f5d8a2470e9eb
shivavaddagani <66823848+shivavaddagani@users.noreply.github.com>
2021-04-07 00:13:20 +0530

    added a file on chapter 2 : strings

--- Exercises_Workout_Strings.py
+++ Exercises_Workout_Strings.py
@@ -1,9 +1,23 @@
-# Chapter 2 : Strings
+                                     # Chapter 2 : Strings
+# ======================= Problem 1 ==================
 Word = input("Enter a word to convert")
 
 def pig_latin(Word):
     if Word[0] in "aeiou":
-        print("{}way".format(Word))
+        return("{}way".format(Word))
     else:
-        print("{}{}ay".format(Word[1::],Word[0]))
-pig_latin(Word)
+        return("{}{}ay".format(Word[1::],Word[0]))
+print(pig_latin(Word))
+
+# ======================= Problem 2 ===================
+sentence = input("enter a sentence")
+list_sentence = sentence.split(" ")            # splitting sentence into words by space delimiter
+
+pig_latin_list = [pig_latin(i) for i in list_sentence]
+new_sentence = ' '.join(pig_latin_list)
+print(pig_latin_list)
+print(new_sentence)
+
+# ======================== Problem 3 ==================
+
+
